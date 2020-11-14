18492b9f5fc4f83f689452b854ee88cad279e638
Candle <thain@s-planet.co.th>
2020-11-14 15:59:30 +0700

    assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  // assetPrefix: isProd ? "https://jjams.co/wallstore" : "",
+  assetPrefix: isProd ? "https://jjams.co/wallstore" : "",
   images: {
     domains: ["localhost"],
   },
