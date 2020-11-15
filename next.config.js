1228d3a537b6625a720bdb78aba3537b1d815a6a
Candle <thain@s-planet.co.th>
2020-11-15 16:29:15 +0700

    fix

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   // assetPrefix: isProd ? "https://jjams.co/wallstore" : "",
-  assetPrefix: "",
-  basePath: "/wallstore",
+  // assetPrefix: "",
+  // basePath: "/wallstore",
   images: {
     domains: ["localhost"],
   },
